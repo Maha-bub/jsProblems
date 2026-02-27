52507159bf3224f9fca7cf06f2025f2e208e1b7a
Your Name <you@example.com>
2026-02-27 21:47:45 +0600

    unite converse

---
+++ converse.js
@@ -0,0 +1,21 @@
+// 12 inch = 1 feet
+
+// kilo meter to mile convert
+
+function kiloToMile(kilo){
+    const mile = kilo/0.6213711922;
+    return mile;
+}
+const kilo = 2;
+let mile =kiloToMile(kilo);
+console.log(mile);
+
+
+
+// mile to kilometer
+function kiloToMile(mile){
+    let kilo = mile*1.609344;
+    return kilo;
+}
+let kilo =kiloToMile(2);
+console.log(kilo);
