39193b189e9e4dbd666008fd67a96990cc0e676f
Your Name <you@example.com>
2026-02-28 22:32:56 +0600

    basic math related function

---
+++ math.js
@@ -0,0 +1,17 @@
+console.log(Math.max(12,14,45,28,87));
+console.log(Math.min(12,14,45,28,87));
+ const math ={
+    min:function min(num1,num2){
+        return num1;
+    }
+ }
+ console.log(Math.PI);
+ console.log(Math.sqrt(4));
+ console.log(Math.abs(45-12));
+ console.log(Math.round(28.15));
+ console.log(Math.round(28.78));
+ console.log(Math.random());
+ console.log(Math.random()*14);
+ console.log(Math.floor(5.58));
+ console.log(Math.ceil(5.00001));
+
