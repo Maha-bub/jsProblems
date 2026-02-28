d00952e12ba056743020f2f8bec371452fcb7816
Your Name <you@example.com>
2026-02-28 23:05:10 +0600

    basic math function

--- math.js
+++ math.js
@@ -7,11 +7,12 @@ console.log(Math.min(12,14,45,28,87));
  }
  console.log(Math.PI);
  console.log(Math.sqrt(4));
- console.log(Math.abs(45-12));
+ console.log(Math.abs(5-12));
  console.log(Math.round(28.15));
  console.log(Math.round(28.78));
  console.log(Math.random());
  console.log(Math.random()*14);
  console.log(Math.floor(5.58));
  console.log(Math.ceil(5.00001));
+
 
