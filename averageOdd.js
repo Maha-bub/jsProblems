56d856d177f95d77d7de1fdbd6ad204f830bcbee
Mahabu-bub <mdmahabubulalam0511@gmail.com>
2026-02-28 09:18:34 +0600

    adding average of an array elements

---
+++ averageOdd.js
@@ -0,0 +1,8 @@
+function oddAverage(numbers){
+
+    console.log(numbers);
+
+}
+const numbers =[24,21,14,15,13,16]
+const avg = oddAverage(numbers);
+console.log("Average of the odd numbers is:",avg);
