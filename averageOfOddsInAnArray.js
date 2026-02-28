7f1c0745b24ffcb4f25d87aedc8835e776e21207
Your Name <you@example.com>
2026-02-28 17:53:55 +0600

    storage odd number in an array and find the average

--- averageOfOddsInAnArray.js
+++ averageOfOddsInAnArray.js
@@ -1,5 +1,5 @@
 function averageOfOdds(numbers) {
-  let sum = 0;
+  
   let oddsNum=[];
   for (const num of numbers) {
     if (num % 2 === 1) {
@@ -9,9 +9,19 @@ function averageOfOdds(numbers) {
     
 
   }console.log(oddsNum);
+
+  let sum = 0;
+  for (const num of oddsNum){
+    sum = sum+num;
+  }
+
+  const count = oddsNum.length;
+  const average = sum/count;
+  return average;
  
 
 }
 
 const numbers = [12, 13, 25, 41, 42];
 const sumOfOdds = averageOfOdds(numbers);
+console.log(sumOfOdds);
