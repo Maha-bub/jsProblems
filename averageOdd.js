7b35f6fb313431aceecdd8cc2136d673f6678384
Mahabu-bub <mdmahabubulalam0511@gmail.com>
2026-02-28 12:14:50 +0600

    average

--- averageOdd.js
+++ averageOdd.js
@@ -1,8 +1,18 @@
 function oddAverage(numbers){
 
-    console.log(numbers);
+    
+    let sum =0;
+    let count= 0;
+    for(const number of numbers){
+        
+        if(number%2===1){
+            sum=sum+number;
+            count=number.length;
+        }
+    }return sum;
 
 }
 const numbers =[24,21,14,15,13,16]
 const avg = oddAverage(numbers);
+coun
 console.log("Average of the odd numbers is:",avg);
