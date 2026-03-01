6de5f89827ef534120c26becb946bd5756edd3ee
Mahabu-bub <mdmahabubulalam0511@gmail.com>
2026-03-01 09:59:34 +0600

    find a tallest or lowest person from an array elements

--- tallest_person.js
+++ tallest_person.js
@@ -1,4 +1,5 @@
-const height =[140,241,15,124,178,145,125,147,123,85,156,250];
+// find a tallest person from in an array
+const height =[140,241,15,124,178,145,125,147,123,85,156, 10, 250];
 function getMaxHeight(talest){
     let maximum = talest[0];
     for(const tall of talest){
@@ -12,3 +13,17 @@ function getMaxHeight(talest){
 
 const maxHeight = getMaxHeight(height)
 console.log(maxHeight);
+
+//  find a lowest person from in an array elements
+function getMinHeight (lowest){
+    let minimum = lowest[0];
+    for(const min of lowest){
+        if(min<=minimum){
+            minimum=min;
+        }
+    }return minimum
+
+}
+const minHeight = getMinHeight(height);
+
+console.log(minHeight);
