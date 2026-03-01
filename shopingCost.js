26d24c8e2fd5b2182ae31c5ba917323dce7190f6
Your Name <you@example.com>
2026-03-01 22:29:35 +0600

    update some instructions

--- shopingCost.js
+++ shopingCost.js
@@ -1,16 +1,16 @@
-const products=[
-    { name:'shampo',price:240,Quantity:4},
-    {name:'shoap',price:200, Quantity:1},
-    {name:'Oil',price:350,Quantity:2},
-    {name:'Chiruni',price:100, Quantity:3}
+const products = [
+  { name: "shampo", price: 240, Quantity: 4 },
+  { name: "shoap", price: 200, Quantity: 1 },
+  { name: "Oil", price: 350, Quantity: 2 },
+  { name: "Chiruni", price: 100, Quantity: 3 },
 ];
-function totalShopingCost(products){
-    let totalPrice =0;
-    for(const product of products){
+function totalShopingCost(products) {
+  let totalPrice = 0;
+  for (const product of products) {
     //    const perProductPrice=product.price*product.Quantity;
-        totalPrice=totalPrice+product.price*product.Quantity;
-    }
-    return totalPrice;
+    totalPrice = totalPrice + product.price * product.Quantity;
+  }
+  return totalPrice;
 }
 
 const total = totalShopingCost(products);
