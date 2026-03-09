41894c09aea691d6250f7924b6d3c8806b5ec7bd
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-03-09 21:37:26 +0600

    iife function used and function expression

--- IIFE_Function.js
+++ IIFE_Function.js
@@ -5,3 +5,14 @@ let sum=0;
     sum=a+b;
     console.log(sum);
 })(5,15);
+
+
+// function expression
+// when a function is assigned a value of a variable , this is called function expression
+
+const displayMessage= function displaymsg(){
+    console.log("I am Mahabub Alam")
+
+
+}
+displayMessage();
