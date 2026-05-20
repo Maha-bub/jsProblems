46e4090f134547d56ec502b218871355d61f606f
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-05-20 14:53:46 +0600

    javascript concepts

--- async.js
+++ async.js
@@ -2,11 +2,15 @@ console.log(1);
 console.log(2);
 // console.log(3);
 setTimeout(() => {
-    doSomething();
+  doSomething();
 }, 3000);
 
 console.log(4);
 console.log(5);
+
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
 console.log(6);
 
 function doSomething() {
