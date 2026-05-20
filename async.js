cd6a3619499c09e15cd786a039a6f7b039f75f71
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-05-20 14:25:13 +0600

    javascript concepts of synchoronous and async

--- async.js
+++ async.js
@@ -1,6 +1,14 @@
 console.log(1);
 console.log(2);
-console.log(3);
+// console.log(3);
+setTimeout(() => {
+    doSomething();
+}, 3000);
+
 console.log(4);
 console.log(5);
 console.log(6);
+
+function doSomething() {
+  console.log(3);
+}
