42dd0dcd609b1e0189e4cca0d5842b5543436a41
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-07-03 07:24:00 +0600

    solve 3rd problem bug

---
+++ debug_problems/pb3.js
@@ -0,0 +1,14 @@
+// function square(num) {
+//   return num + num; // find the bug
+// }
+ 
+// console.log(square(4)); // should be 16
+
+
+function square(num){
+    return num*num;
+}
+
+const squareValue=square(3);
+console.log(squareValue);
+console.log(square(5));
