bb32527f7f6636f5d9101bfa93dbacc181e7c3d0
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-07-03 07:17:29 +0600

    fix first two problem bug

---
+++ debug_problems/pb-2.js
@@ -0,0 +1,7 @@
+// let word = "hello";
+// console.log(word.toUppercase()); // find the bug
+
+
+ let word = "hello";
+ const uppercaseWord=word.toUpperCase();
+ console.log(uppercaseWord);
---
+++ debug_problems/problem-1.js
@@ -0,0 +1,11 @@
+// function sayHi(name) {
+//   console.log("Hi, " + name  // find the bug
+// }
+ 
+// sayHi("Mira");
+
+// solutions:
+function sayHi(name){
+    console.log("Hi,",name);
+}
+const greating =sayHi("Mahabub");
