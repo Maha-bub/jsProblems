dadcae37955d276ff594d393f3686ae92a1a4c95
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-08-06 00:17:54 +0600

    Update arrow function

--- ES6/receipt_generator.js
+++ ES6/receipt_generator.js
@@ -1,8 +1,13 @@
-function receiptGenerator(productName, price, qty) {
+// function receiptGenerator(productName, price, qty) {
+//     let totalPrice = price * qty
+//     return `${productName} x${qty} = ${totalPrice} Taka`;
+// }
+
+const receiptGenerator = (productName, price, qty) => {
+
     let totalPrice = price * qty
     return `${productName} x${qty} = ${totalPrice} Taka`;
 }
-
 
 console.log(receiptGenerator('pen', 10, 4))
 
