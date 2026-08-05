796337d8ae45c36011d60d573d04b7ea49a1dcfb
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-08-06 00:13:02 +0600

    get input from user and return total amount of account

---
+++ ES6/receipt_generator.js
@@ -0,0 +1,11 @@
+function receiptGenerator(productName, price, qty) {
+    let totalPrice = price * qty
+    return `${productName} x${qty} = ${totalPrice} Taka`;
+}
+
+
+console.log(receiptGenerator('pen', 10, 4))
+
+console.log(receiptGenerator('Book', 15, 4))
+
+console.log(receiptGenerator('pen', 154, 4))
