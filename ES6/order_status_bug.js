e03f4074a6b3cb5735545b30d9f4657c97c814f3
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-08-09 23:59:37 +0600

    Es 6 prblem

---
+++ ES6/order_status_bug.js
@@ -0,0 +1,7 @@
+var status = "order placed";
+for (let i = 1; i <= 3; i++) {
+    let status = "Processing Item" + i;
+    console.log(status);
+}
+console.log(status);
+console.log("Final status:", status)
