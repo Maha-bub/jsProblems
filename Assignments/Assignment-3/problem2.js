f08535758b5486e4c8b9c90967748696ff0b623a
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-08-10 21:50:03 +0600

    Active User list filtered

--- Assignments/Assignment-3/problem2.js
+++ Assignments/Assignment-3/problem2.js
@@ -1,6 +1,6 @@
 const filterActiveUsers = (users) => {
     // console.log(users)
-    if (Array.isArray(users) == false || users.length === 0 || Object.keys(users) != 'isActive') {
+    if (Array.isArray(users) == false || users.length === 0 || Object.keys(users).includes('isActive')) {
         return "Invalid";
     }
     let activeUsers = users.filter((user) => {
@@ -19,7 +19,14 @@ let userList = [
     { name: 'Tanjil', isActive: false },
     { name: 'Polok' }
 ]
-// console.log(filterActiveUsers(54))
-// console.log("Empty array Provided:", filterActiveUsers([]))
+
+let userList1 = [
+    { name: "A", isActive: true },
+    { name: "B", isActive: false }
+]
+console.log(filterActiveUsers(54))
+console.log("Empty array Provided:", filterActiveUsers([]))
 console.log(filterActiveUsers(userList))
-// console.log(filterActiveUsers('userList'))
+
+console.log(filterActiveUsers(userList1))
+console.log(filterActiveUsers('userList1'))
