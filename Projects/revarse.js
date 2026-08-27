3ee5f4b9e73df5f9858acba501b8d46ef2a7c9cf
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-08-27 14:34:53 +0600

    js advanced topic

---
+++ Projects/revarse.js
@@ -0,0 +1,8 @@
+const promise='I am Mahabubul Alam , I want to be a skilled web developer';
+const char=promise.split('');
+console.log(char);
+
+const revarsechar=char.reverse();
+console.log(revarsechar);
+const revarsePromise=revarsechar.join('');
+console.log(revarsePromise);
